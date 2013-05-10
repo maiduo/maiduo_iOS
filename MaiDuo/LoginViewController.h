4143c615948f90a61585e1ab4d4e8ecfec3f8c2f
knight731 <knight731@hotmail.com>
2013-05-10 23:08:47 +0800

    增加了登录页面

---
+++ MaiDuo/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  MaiDuo
+//
+//  Created by 高 欣 on 13-5-10.
+//  Copyright (c) 2013年 魏琮举. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UITableViewController<UITextFieldDelegate>
+
+@end
