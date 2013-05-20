5b22f7273bc1828f737b3e852560dafe7be64323
CJ <weicongju@gmail.com>
2013-05-20 17:23:12 +0800

    添加说明。

--- MaiDuo/MDNotificationCenter.h
+++ MaiDuo/MDNotificationCenter.h
@@ -14,6 +14,34 @@
 #define kDidReceiveChat @"didReceiveChat"
 #define kDidReceiveActivity @"didReceiveActivity"
 
+/** MDNotificationCenter 推送中心
+ 
+ 和NSNotificationCenter有很大的不同，MDNotificationCenter并不是单例类，主要是反序列化推
+送的数据成为对象，并传递给NSNotificationCenter。
+ 
+ MDAppDelegate.m
+ 
+     - (void)application:(UIApplication *)application
+     didReceiveRemoteNotification:(NSDictionary *)userInfo
+     {
+        // MDNotificationCenter
+        [_notification post:userInfo];
+     }
+ 
+ ChatViewController.m
+ 
+    - (void)pullReceiveChat:(id)sender
+    {
+        MDChat *chat = (MDChat *)[[sender userInfo] objectForKey:@"object"];
+    }
+ 
+    - (void)viewDidLoad {
+         [[NSNotificationCenter defaultCenter] addObserver:self
+          selector:@selector(pullReceiveChat:)
+          name:kDidReceiveChat
+          object:nil];
+    }
+ */
 @interface MDNotificationCenter : NSObject {
     MDCache *_cache;
 }
